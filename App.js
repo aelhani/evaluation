71675d26ed249fa9b215cea43f69e840410c9d83
aelhani <abderrazak.elhani@gmail.com>
2019-01-29 13:04:56 +0100

    revision#1

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+// ---------------------------------------------------------
+// Evaluation : Application d’authentification de contact
+// Réalisée par Abderrazak Elhani (pour Chmarkh Kaoutar)
+// ** React Native Specialist
+// ** https://www.linkedin.com/in/elhani
+// ** (+212) 653 68 65 68
+// ---------------------------------------------------------
 
 import React from 'react'
 
